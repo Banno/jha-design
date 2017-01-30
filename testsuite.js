78841762b8494cd4be3c9439ff13623a3dfa3b28
Josh Sadler <josh.sadler@banno.com>
2017-01-30 14:29:33 -0600

    remove testsuite

--- testsuite.js
+++
@@ -1,8 +0,0 @@
-casper.
-    start('http://localhost:7777').
-    then(function() {
-        // Required elements
-        phantomcss.screenshot('.navigation', 'Navigation');
-        phantomcss.screenshot('.tab-bar', 'Tab Bar');
-        phantomcss.screenshot('.page-header', 'Page Header');
-    });
